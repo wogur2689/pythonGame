a1c068848e4fe1dd3934990f025a7f397c999e11
jaehyeokGil <81075170+wogur2689@users.noreply.github.com>
2022-06-19 15:17:45 +0900

    feat: 기본세팅

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+# 파이게임 불러오기
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# 파이게임 초기화
+pygame.init()
 
+# 크기 지정
+screen_width = 500
+screen_height = 500
+screen = pygame.display.set_mode((screen_width, screen_height))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# 색상 지정
+WHITE = (255, 255, 255)
 
+# 제목 지정
+pygame.display.set_caption("파이게임")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# 종료 플래그로 사용되는 변수
+done = False
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 메인 루프
+while not done:
+    for event in pygame.event.get(): # 여러 이벤트를 받아 처리
+        if event.type == pygame.QUIT: # 윈도 종료 버튼을 누르면 종료
+            done = True
+
+        screen.fill(WHITE) #스크린 색상 흰색
+        pygame.display.update() #화면의 필요한 부분만을 수정
+
+# 파이게임 종료
+pygame.quit()
