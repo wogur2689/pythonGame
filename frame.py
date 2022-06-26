9d93255b922808e9cda49afc0507d9264a28d6a2
jaehyeokGil <81075170+wogur2689@users.noreply.github.com>
2022-06-26 14:23:44 +0900

    REFACTOR: 기본세팅 수정

--- frame.py
+++ frame.py
@@ -1,30 +1,31 @@
-# 파이게임 불러오기
+# 배경 이미지 설정
 import pygame
 
 # 파이게임 초기화
 pygame.init()
 
 # 크기 지정
-screen_width = 500
-screen_height = 500
+screen_width = 448
+screen_height = 720
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-# 색상 지정
-WHITE = (255, 255, 255)
-
 # 제목 지정
-pygame.display.set_caption("파이게임")
+pygame.display.set_caption("Puzzle Bobble")
+
+# 게임시간 설정
+clock = pygame.time.Clock()
 
 # 종료 플래그로 사용되는 변수
-done = False
+running = True
 
 # 메인 루프
-while not done:
+while running:
+    clock.tick(60) # FPS 60으로 설정
+
     for event in pygame.event.get(): # 여러 이벤트를 받아 처리
         if event.type == pygame.QUIT: # 윈도 종료 버튼을 누르면 종료
-            done = True
+            running = False
 
-        screen.fill(WHITE) #스크린 색상 흰색
         pygame.display.update() #화면의 필요한 부분만을 수정
 
 # 파이게임 종료
